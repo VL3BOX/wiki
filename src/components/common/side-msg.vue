23bb04da6c907c7be47b2e27a7f6a9800c14dd5c
fifththirteen <570205219@qq.com>
2023-12-15 17:44:36 +0800

    fix:成就群

---
+++ src/components/common/side-msg.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="c-wiki-side-msg">
+        <RightSideMsg>
+            <em>魔盒成就群</em> :
+            <strong>
+                <a href="https://jq.qq.com/?_wv=1027&k=5S50j08">{{ qq }}</a>
+            </strong>
+        </RightSideMsg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CommonSideMsg",
+    data() {
+        return {
+            qq: "614370825",
+        };
+    },
+};
+</script>
+
+<style lang="less"></style>
--- src/service/group.js
+++ src/service/group.js
@@ -1,9 +1,12 @@
-import { $helper } from "@jx3box/jx3box-common/js/https";
+import { $helper, $cms } from "@jx3box/jx3box-common/js/https";
 
 function get_groups(type, params) {
     return $helper().get(`api/groups/${type}`, {
         params: params,
     });
 }
+function getMenuGroup(name) {
+    return $cms({ mute: true }).get(`/api/cms/menu-group/${name}`, {});
+}
 
-export { get_groups };
+export { get_groups, getMenuGroup };
