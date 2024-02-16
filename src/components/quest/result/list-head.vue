f50239959d8b4b499ffb4cb9108c989ea4d0731f
VL3BOX <admin@vl3box.com>
2024-02-16 20:13:56 +0700

    tran: fix i18n translation in functional components

--- src/components/quest/result/list-head.vue
+++ src/components/quest/result/list-head.vue
@@ -1,10 +1,10 @@
 <template functional>
     <div class="m-list-head">
-        <div>{{ $t('起始地图') }}</div>
-        <div>{{ $t('任务名称') }}</div>
-        <div>{{ $t('可接等级') }}</div>
-        <div>{{ $t('任务目标') }}</div>
-        <div>{{ $t('任务奖励') }}</div>
+        <div>{{ parent.$t('起始地图') }}</div>
+        <div>{{ parent.$t('任务名称') }}</div>
+        <div>{{ parent.$t('可接等级') }}</div>
+        <div>{{ parent.$t('任务目标') }}</div>
+        <div>{{ parent.$t('任务奖励') }}</div>
     </div>
 </template>
 
